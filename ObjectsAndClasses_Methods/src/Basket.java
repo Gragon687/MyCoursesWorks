5c7cee79ba185198f056ce7a808292457efbbe3c
Ilya Polilov <polilov2004@mail.ru>
2022-10-03 11:09:09 +0300

    Task 6.8 #1

--- ObjectsAndClasses_Methods/src/Basket.java
+++ ObjectsAndClasses_Methods/src/Basket.java
@@ -4,12 +4,22 @@ public class Basket {
     private String items = "";
     private int totalPrice = 0;
     private int limit;
-    private  double totalWeight = 0;
+    private double totalWeight = 0;
+    private static int totalItemsCount = 0;
+    private static int priceOfAllBaskets = 0;
+
+    public static int getTotalItemsCount() {
+        return totalItemsCount;
+    }
+
+    public static int getPriceOfAllBaskets() {
+        return priceOfAllBaskets;
+    }
 
     public Basket() {
         increaseCount(1);
         items = "Список товаров:";
-        this.limit = 1000000;
+        this.limit = 999999000;
     }
 
     public Basket(int limit) {
@@ -30,9 +40,24 @@ public class Basket {
     public static void increaseCount(int count) {
         Basket.count = Basket.count + count;
     }
+    public static void items(int count){
+        Basket.totalItemsCount += count;
+    }
+    public  static  void allBasketsPrice(int price, int count){
+        Basket.priceOfAllBaskets += (price * count);
+    }
 
+    public static int averagePriceOfItems(){
+        int averagePrice = Basket.getPriceOfAllBaskets() / Basket.getTotalItemsCount();
+        return averagePrice;
+    }
+    public static int averagePriceOfBaskets(){
+        int averagePrice = Basket.getPriceOfAllBaskets() / Basket.count;
+        return averagePrice;
+    }
     public void add(String name, int price) {
         add(name, price, 1);
+
     }
 
     public void add(String name, int price, int count) {
@@ -51,11 +76,15 @@ public class Basket {
         }
 
         items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
+                count + " шт. - " + price;
         totalPrice = totalPrice + count * price;
+        items(count);
+        allBasketsPrice(price, count);
+
     }
-    public void add(String name, int price, int count, double weight){
-        if (count == 1){
+
+    public void add(String name, int price, int count, double weight) {
+        if (count == 1) {
             add(name, price);
             totalWeight += weight;
         } else {
@@ -73,7 +102,8 @@ public class Basket {
     public int getTotalPrice() {
         return totalPrice;
     }
-    public double getTotalWeight(){
+
+    public double getTotalWeight() {
         return totalWeight;
     }
 
--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -4,8 +4,19 @@ public class Main {
         Basket basket = new Basket();
         basket.add("Milk", 40, 3, 14);
         basket.add("Coffee", 50, 10, 5);
-        basket.print("Milk");
-        double weight =basket.getTotalWeight();
-        System.out.println(weight);
+        basket.print("First Basket");
+        System.out.println(basket.getTotalWeight());
+
+        Basket basket1 = new Basket();
+        basket1.add("Milk", 40, 10, 14);
+        basket1.add("Coffee", 50, 15, 5);
+        basket1.print("Second Basket");
+
+        System.out.println(Basket.getPriceOfAllBaskets());
+        System.out.println(Basket.getTotalItemsCount());
+        System.out.println(Basket.averagePriceOfBaskets());
+        System.out.println(Basket.averagePriceOfItems());
+
+
     }
 }
