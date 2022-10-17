f61e29b9fc0cf5e4457497667f93605ffcedd759
Ilya Polilov <polilov2004@mail.ru>
2022-10-17 14:08:35 +0300

    Second task!

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -8,8 +8,11 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
-        for(char i = 'А'; (i >= 'А' && i <= 'я'); i++){
-            int code = (int) i;
+        for(int i = 0; i < 65536; i++){
+            char symbol = (char) i;
+            if ((symbol >= 'А' && symbol <= 'я') || (symbol == 'Ё') || (symbol == 'ё')){
+                System.out.println(symbol + "=" + i);
+            }
         }
 
     }
